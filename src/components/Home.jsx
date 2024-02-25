ad3ec57c86403c80fd0bee517b9b2f24a7e86459
priyankaj04 <priyankaj2002o4@gmail.com>
2024-02-25 12:25:42 +0530

    home ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,24 @@
 import React, { useRef, useEffect, useState } from 'react'
 import Spline from '@splinetool/react-spline';
+import logo from '../images/logo.png'
 
 function Home() {
 
   const [hover, setHover] = useState(false);
   return (
     <div id="home" className='h-screen flex-col lg:flex-row xl:flex-row 2xl:flex-row md:flex-col sm:flex-col justify-center flex-1 items-center'>
-      <div className='flex-1 mt-16 text-3xl'>
-        <h3 className='font-bold text-center text-green-400' onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>Hello There!</h3>
-        <h2 className='font-bold text-center text-white' onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>We are TechTangoo ⊙⊙</h2>
+      <div className='justify-center flex-1 mt-16 text-3xl'>
+        <h3 className='font-bold text-center text-green-400' onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>Hell⊙ There!</h3>
+        <h2 className='font-bold text-center text-white flex justify-center' onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>We are TechTang
+          <span>
+            <img src={logo} width='40px' className='p-0 m-0 mt-2' /></span></h2>
+      </div>
+      <div className='mt-10'>
+        <h2 className='text-green-400 font-bold text-center text-2xl mb-0'>Transforming Ideas into Impactful Designs</h2>
+        <p className='text-white font-bold text-md text-center mt-0'>
+          <span className='text-green-200 text-lg'>From Concept to Creation, Your Vision, Our Expertise.</span><br />
+          We are dedicated to providing unparalleled service tailored to your business's unique needs.
+        </p>
       </div>
       {/*<Spline scene="https://prod.spline.design/adabPntTxRtyYAhD/scene.splinecode" />*/}
     </div>
